21cdcfe087e5c4e8095397101d0e9cf867bea73e
grzeslaff <grzeslaff@clumaster.astro.amu.edu.pl>
2017-10-30 18:11:05 +0100

    zgubiony scheduler.cpp dodany

---
+++ src/scheduler.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <vector>
+
+#include "MessageQueue.h"
+#include "Job.h"
+
+#define MAXSIZE 128
+
+using namespace std;
+
+
+
+int main() {
+
+
+	MessageQueue msq(1234);
+
+	vector<Job> jobs;
+	while (1)
+	{
+		//czytaj wszystkie dostępne komunikaty czekające w kolejce
+		vector<string> messages = msq.readQueue();
+		for (auto m: messages)
+			jobs.push_back( Job(m) );
+
+
+
+
+		char c = getchar();
+
+		if (c == 'q')
+			break;
+		else if (c == 'p')
+			for (auto j: jobs)
+				j.printInfo();
+
+
+	}
+
+
+
+	return 0;
+}
+
